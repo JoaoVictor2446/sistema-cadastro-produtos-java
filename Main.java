013a3226c558dcdf992502404fef4b302c52969e
JoaoVictor2446 <joao30braga@gmail.com>
2026-03-27 09:31:53 -0300

    Create Main.java
    Este projeto consiste em um sistema de cadastro de produtos desenvolvido em Java, com o objetivo de praticar conceitos fundamentais de programação, como estruturas de repetição, listas (ArrayList) e Programação Orientada a Objetos.
    
    O sistema permite realizar operações de CRUD (Create, Read, Update e Delete), incluindo:
    
    - Cadastro de produtos com validação de ID único
    - Listagem de todos os produtos cadastrados
    - Busca de produtos por ID
    - Remoção de produtos
    
    Durante o desenvolvimento, foram aplicados conceitos como organização de código em métodos, manipulação de entrada de dados com Scanner e controle de fluxo.
    
    Este projeto foi desenvolvido como parte dos estudos em Java com foco em evolução para desenvolvimento profissional.

---
+++ Main.java
@@ -0,0 +1,106 @@
+package Projetos;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+    int num =1;
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Produto>produtosArrayList=new ArrayList<>();
+    while (num!=0) {
+        System.out.println("1 - Cadastrar produto\n" + 
+                          "2 - Listar produto\n" + 
+                          "3 - Buscar produto pelo id\n"+ 
+                          "4 - Remover produto pelo id\n" + 
+                          "0 - Sair\n");
+     num=scanner.nextInt();
+      scanner.nextLine(); 
+     boolean achou=false;      
+if (num==1) { cadastrar(produtosArrayList, scanner);
+   }
+if (num==2) { listar(produtosArrayList);
+    } 
+if (num==3) { buscar(produtosArrayList, scanner);
+    }
+if (num==4) { remover(produtosArrayList, scanner);
+    }}
+scanner.close();
+}   
+
+  public static void  cadastrar(ArrayList<Produto> cadastrarList, Scanner scanner) {
+    int cadas;
+ System.out.println("Quantos produtos você quer cadastrar");
+    cadas= scanner.nextInt();
+    scanner.nextLine();
+    for (int i = 0; i < cadas; i++) {
+        Produto produto= new Produto();      
+   System.out.println("\nProduto sendo cadastrado " + (i + 1));
+   System.out.println("Nome do produto");
+   produto.setNome(scanner.nextLine());
+   System.out.println("Preco do produto");
+   produto.setPreco(scanner.nextDouble());
+   scanner.nextLine();
+ int id;
+boolean idExiste;
+
+do {
+    idExiste = false;
+    System.out.println("Diga o ID do produto");
+    id = scanner.nextInt();
+    scanner.nextLine();
+    for (Produto p : cadastrarList) {
+        if (p.getId() == id) {
+            System.out.println("Esse ID já existe, digite outro.");
+            idExiste = true;
+            break;}}}   
+
+while (idExiste);
+produto.setId(id);
+
+   cadastrarList.add(produto); }  }
+    
+ public static void listar(ArrayList<Produto> listaList){
+    if (listaList.isEmpty()) {
+        System.out.println("Nenhum produto cadastrado, por favor cadastre o um produto primeiro");
+      }   
+    else{
+        for (Produto produto : listaList) {
+        System.out.println(produto);}}}
+    
+public static void buscar(ArrayList<Produto> buscar, Scanner scanner){
+    boolean achou= false;
+  if (buscar.isEmpty()) {
+        System.out.println("Nenhum produto cadastrado, por favor cadastre o um produto primeiro");}
+    else{
+        System.out.println("Coloque o ID do produto que você quer procurar");
+        int busc = scanner.nextInt();
+        scanner.nextLine();
+  for (Produto produto : buscar){
+    if (produto.getId()== busc) {
+        System.out.println("Produto encontrado");
+        System.out.println(produto); 
+    achou=true;}}
+   if(achou==false){System.out.println("Produto não encontrado");}}
+}
+public static void remover(ArrayList<Produto> removerList, Scanner scanner){
+    boolean achou = false;
+  if (removerList.isEmpty()) { 
+        System.out.println("Nenhum produto cadastrado, por favor cadastre o um produto primeiro");}
+    else{
+    System.out.println("Coloque o ID do produto que você quer remover");
+    int buscar=scanner.nextInt();
+        for (int i = 0; i < removerList.size(); i++) {
+        if (removerList.get(i).getId() == buscar) {
+        System.out.println("Produto removido");
+        removerList.remove(i);
+         achou=true;
+        break;}}
+          if(achou==false){System.out.println("Produto não encontrado");
+       }}}} 
+
+  
+    
+    
+    
+
+
+  
